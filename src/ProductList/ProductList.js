4e91516e08bf8757360fdf666867abb794d4dfdc
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2023-07-16 20:19:13 +0530

    code

--- src/ProductList/ProductList.js
+++ src/ProductList/ProductList.js
@@ -4,26 +4,32 @@ import './ProductList.css';
 
 const products = [
     {
+        id: 1,
         title: 'Apple iPhone 14',
         price: 'Rs. 1,00,000'
     },
     {
+        id: 2,
         title: 'Apple iPhone 13',
         price: 'Rs. 70,000'
     },
     {
+        id: 3,
         title: 'Google Pixel 7',
         price: 'Rs. 50,000'
     },
     {
+        id: 4,
         title: 'Nokia 1100',
         price: 'Rs. 2,000'
     },
     {
+        id: 5,
         title: 'Samsung Galaxy S10',
         price: 'Rs. 1,00,000'
     },
     {
+        id: 6,
         title: 'Sony Xperia S10',
         price: 'Rs. 1,00,000'
     }
@@ -42,15 +48,17 @@ const ProductList = () => {
 
     useEffect(()=> {
         getProductsApi(function(res) {
+            console.log('API started');
             setProducts(res);
             setIsLoading(false);
+            console.log('API ended');
         });
-    },[]);
+    },[products, isLoading]);
 
 
     const productList = products.map((product) => {
         return (
-            <ProductCard key = {product.title} title = {product.title} price = {product.price}/>
+            <ProductCard key = {product.id} title = {product.title} price = {product.price}/>
         );
     });
 
